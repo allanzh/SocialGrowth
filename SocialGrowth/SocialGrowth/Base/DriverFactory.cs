eaed30fba194529f3f5384356adf55db9eeaf434
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2020-06-20 23:34:03 -0300

    Add factory do driver

---
+++ SocialGrowth/SocialGrowth/Base/DriverFactory.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialGrowth.Base
+{
+    public class DriverFactory
+    {
+        private static IWebDriver driver = null;
+
+        private static void CreateDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static IWebDriver GetDriver() 
+        {
+            if (driver == null) CreateDriver();
+            
+            return driver;
+        }
+
+
+        public static void DestroyDriver()
+        {
+            driver.Close();
+        }
+
+    }
+}
--- SocialGrowth/SocialGrowth/Main.cs
+++ SocialGrowth/SocialGrowth/Main.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SocialGrowth.Base;
 
 namespace SocialGrowth
 {
     [TestClass]
     public class Main
     {
+        public IWebDriver driver;
+
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
