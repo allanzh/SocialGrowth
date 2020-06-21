10f496fd859d380d42fda3e3da998134e97baaeb
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2020-06-21 02:06:11 -0300

    add mainpageobjects

---
+++ SocialGrowth/SocialGrowth/Base/MainpageObjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialGrowth.Base
+{
+    public class MainpageObjects : TestBase
+    {
+        //  //button[text()='Agora não']
+        //  //input[@placeholder='Pesquisar']
+        //  //a[@href='/']
+    }
+}
